a43f4150e27b4240b61ebe58ce595b95017aa2a2
Mark Cyrell Quinio <iamsayril@gmail.com>
2026-06-25 10:17:59 +0800

    Add files via upload

--- im102_quinio_db.sql
+++ im102_quinio_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2026 at 04:22 AM
+-- Generation Time: Jun 25, 2026 at 04:17 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -57,26 +57,29 @@ CREATE TABLE `products` (
   `stock` int(11) NOT NULL,
   `category_id` int(11) DEFAULT NULL,
   `supplier_id` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `added_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `description`, `price`, `stock`, `category_id`, `supplier_id`, `created_at`) VALUES
-(1, 'Wireless Mouse', 'Ergonomic wireless mouse', 499.99, 50, 1, 1, '2026-06-15 01:21:55'),
-(2, 'Mechanical Keyboard', 'RGB backlit keyboard', 1500.00, 30, 1, 1, '2026-06-15 01:21:55'),
-(3, 'Bluetooth Speaker', 'Portable mini speaker', 899.00, 40, 1, 1, '2026-06-15 01:21:55'),
-(4, 'USB Flash Drive 64GB', 'High-speed USB 3.0 storage', 350.00, 100, 1, 1, '2026-06-15 01:21:55'),
-(5, 'Chocolate Cake', 'Moist chocolate layered cake', 650.00, 20, 3, 2, '2026-06-15 01:21:55'),
-(6, 'Banana Bread', 'Freshly baked banana loaf', 180.00, 35, 3, 2, '2026-06-15 01:21:55'),
-(7, 'Croissant', 'Buttery flaky pastry', 75.00, 50, 3, 2, '2026-06-15 01:21:55'),
-(8, 'Box of Cupcakes', '6 assorted cupcakes', 300.00, 25, 3, 2, '2026-06-15 01:21:55'),
-(9, 'Fresh Milk 1L', 'Organic farm fresh milk', 90.00, 60, 3, 3, '2026-06-15 01:21:55'),
-(10, 'Eggs (Dozen)', 'Free-range chicken eggs', 120.00, 80, 3, 3, '2026-06-15 01:21:55'),
-(11, 'Carrots 1kg', 'Fresh organic carrots', 70.00, 100, 3, 3, '2026-06-15 01:21:55'),
-(12, 'Lettuce', 'Crisp green lettuce', 50.00, 90, 3, 3, '2026-06-15 01:21:55');
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `stock`, `category_id`, `supplier_id`, `created_at`, `added_by`) VALUES
+(1, 'Wireless Mouse', 'Ergonomic wireless mouse', 499.99, 50, 1, 1, '2026-06-15 01:21:55', 1),
+(2, 'Mechanical Keyboard', 'RGB backlit keyboard', 1500.00, 30, 1, 1, '2026-06-15 01:21:55', 1),
+(3, 'Bluetooth Speaker', 'Portable mini speaker', 899.00, 40, 1, 1, '2026-06-15 01:21:55', 1),
+(4, 'USB Flash Drive 64GB', 'High-speed USB 3.0 storage', 350.00, 100, 1, 1, '2026-06-15 01:21:55', 1),
+(5, 'Chocolate Cake', 'Moist chocolate layered cake', 650.00, 20, 3, 2, '2026-06-15 01:21:55', 1),
+(6, 'Banana Bread', 'Freshly baked banana loaf', 180.00, 35, 3, 2, '2026-06-15 01:21:55', 1),
+(7, 'Croissant', 'Buttery flaky pastry', 75.00, 50, 3, 2, '2026-06-15 01:21:55', 1),
+(8, 'Box of Cupcakes', '6 assorted cupcakes', 300.00, 25, 3, 2, '2026-06-15 01:21:55', 1),
+(9, 'Fresh Milk 1L', 'Organic farm fresh milk', 90.00, 60, 3, 3, '2026-06-15 01:21:55', 1),
+(10, 'Eggs (Dozen)', 'Free-range chicken eggs', 120.00, 80, 3, 3, '2026-06-15 01:21:55', 1),
+(11, 'Carrots 1kg', 'Fresh organic carrots', 70.00, 100, 3, 3, '2026-06-15 01:21:55', 1),
+(12, 'Lettuce', 'Crisp green lettuce', 50.00, 90, 3, 3, '2026-06-15 01:21:55', 1),
+(13, 'franco', 'adsad', 2224.00, 19, 4, 3, '2026-06-25 01:24:13', 5),
+(14, 'Banana Cake', 'banana cake', 250.00, 5, 2, 2, '2026-06-25 02:00:50', 6);
 
 -- --------------------------------------------------------
 
@@ -122,7 +125,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `email`, `password_hash`, `role`, `created_at`) VALUES
 (1, 'admin', 'admin@example.com', '$2y$10$BrYJenw8brpRyiPi9TswquhWOGkMZsp7nRP4Kmms6BiIOOnBWo46S', 'admin', '2026-06-24 01:18:35'),
 (2, 'test_staff', 'test_staff@gmail.com', '$2y$10$rOOfkH5r8D1qwju74nWZru9AZxf8FTdSbpWXbwZuAhssBnqVFfobm', 'staff', '2026-06-24 01:21:20'),
-(3, 'test2_staff333', 'test2_staff@gmail.com', '$2y$10$W1zhPUMzP1LDUTLfIKDT4.F.WTvIwagDr5FwPX//aFh2ZKbaylNae', 'staff', '2026-06-24 01:51:21');
+(3, 'test2_staff333', 'test2_staff@gmail.com', '$2y$10$W1zhPUMzP1LDUTLfIKDT4.F.WTvIwagDr5FwPX//aFh2ZKbaylNae', 'staff', '2026-06-24 01:51:21'),
+(5, 'admin_2', 'admin_2@gmail.com', '$2y$10$f7Iw0poe4A4Y9HKsf1EMCu7pgMbTsQhbKrxoaGDozyxLnIWhIzF5y', 'admin', '2026-06-25 01:07:31'),
+(6, 'Cyrell', 'cyrell@gmail.com', '$2y$10$cAhZ.lUti5AZR994Or11jezKHr5orjAOeKonN6yzHNVZIFxu5l9EC', 'admin', '2026-06-25 02:00:25'),
+(7, 'test_m', 'test_m@gmail.com', '$2y$10$mOqIy/HJM7OZmouHf0J2/.UMCicXlcDgo3bNYGRlDOFxisMC6UrQm', 'staff', '2026-06-25 02:16:10');
 
 --
 -- Indexes for dumped tables
@@ -140,7 +146,8 @@ ALTER TABLE `categories`
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD KEY `category_id` (`category_id`),
-  ADD KEY `supplier_id` (`supplier_id`);
+  ADD KEY `supplier_id` (`supplier_id`),
+  ADD KEY `added_by` (`added_by`);
 
 --
 -- Indexes for table `suppliers`
@@ -170,7 +177,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
@@ -182,7 +189,7 @@ ALTER TABLE `suppliers`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -193,7 +200,8 @@ ALTER TABLE `users`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
-  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`);
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`),
+  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`added_by`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
